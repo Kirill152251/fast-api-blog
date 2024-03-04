05beec8802656d8f5079042ce7be5b332e4a09a8
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2024-03-04 21:39:30 +0300

    Create User and comment db models
    Imlement alembic

---
+++ app/db/models.py
@@ -0,0 +1,37 @@
+import enum
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Enum
+from sqlalchemy.orm import relationship
+
+from db.database import Base
+
+
+class UserRole(enum.Enum):
+    user = 'user'
+    admin = 'admin'
+    superuser = 'superuser'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column('id', Integer, primary_key=True)
+    email = Column(String, unique=True, index=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(Enum(UserRole), default=UserRole.user)
+
+    comments = relationship('Comment', back_populates='author')
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id = Column('id', Integer, primary_key=True)
+    text = Column(String)
+    pub_date = Column(DateTime)
+    author_id = Column(Integer, ForeignKey('users.id'))
+
+    author = relationship('User', back_populates='comments')
